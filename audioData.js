b3bfeebb0e3077e4b46889814b0829d018d99a0f
De Roover Justin <justinderoover@gmail.com>
2023-10-24 18:52:25 +0200

    end add track

--- audioData.js
+++ audioData.js
@@ -4,5 +4,25 @@ const audioTracks = [
     title: "J'arrive",
     source: "audio/Jarrive.mp3",
   },
+  {
+    artist: "Angèle [IA]",
+    title: "Saiyan",
+    source: "audio/Saiyan.m4a",
+  },
+  {
+    artist: "Bad Bunny",
+    title: "Monaco",
+    source: "audio/Monaco.m4a",
+  },
+  {
+    artist: "Bad Bunny",
+    title: "Dakiti",
+    source: "audio/Dakiti.m4a",
+  },
+  {
+    artist: "Freeze Corleone",
+    title: "Fraude",
+    source: "audio/Fraude.m4a",
+  },
 ];
 export default audioTracks;
