31d3aa0906f7469a4c16bc0f4ffc371084276d60
De Roover Justin <justinderoover@gmail.com>
2023-10-24 19:01:43 +0200

    f

--- audioData.js
+++ audioData.js
@@ -2,27 +2,27 @@ const audioTracks = [
   {
     artist: "Jewel Usain",
     title: "J'arrive",
-    source: "audio/Jarrive.mp3",
+    source: "images/audio/Jarrive.mp3",
   },
   {
     artist: "Angèle [IA]",
     title: "Saiyan",
-    source: "audio/Saiyan.m4a",
+    source: "images/audio/Saiyan.m4a",
   },
   {
     artist: "Bad Bunny",
     title: "Monaco",
-    source: "audio/Monaco.m4a",
+    source: "images/audio/Monaco.m4a",
   },
   {
     artist: "Bad Bunny",
     title: "Dakiti",
-    source: "audio/Dakiti.m4a",
+    source: "images/audio/Dakiti.m4a",
   },
   {
     artist: "Freeze Corleone",
     title: "Fraude",
-    source: "audio/Fraude.m4a",
+    source: "images/audio/Fraude.m4a",
   },
 ];
 export default audioTracks;
