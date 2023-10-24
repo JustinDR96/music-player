8397dd83d2d2381fd3b177c49a169b3a7d1339de
De Roover Justin <justinderoover@gmail.com>
2023-10-24 19:25:06 +0200

    f

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,25 @@
-import { defineConfig } from 'vite';
-import copy from 'rollup-plugin-copy';
+import { defineConfig } from "vite";
+import copy from "rollup-plugin-copy";
 
 export default defineConfig({
-  base: '/music-player/', // Remplacez '/chemin-de-votre-application/' par la base souhaitée
+  base: "/music-player/", // Remplacez '/chemin-de-votre-application/' par la base souhaitée
 
   build: {
     rollupOptions: {
       output: {
-        dir: 'dist',
+        dir: "dist",
       },
     },
   },
 
   plugins: [
-    // Plugin pour copier le répertoire "audio" dans le répertoire de sortie
+    // Plugin pour copier les répertoires "audio" et "images" dans le répertoire de sortie
     copy({
       targets: [
-        { src: 'audio', dest: 'dist' },
+        { src: "audio", dest: "dist/audio" }, // Copier le répertoire "audio" dans "dist/audio"
+        { src: "images", dest: "dist/images" }, // Copier le répertoire "images" dans "dist/images"
       ],
-      hook: 'writeBundle',
+      hook: "writeBundle",
     }),
   ],
 });
