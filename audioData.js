eca4a3386c2e6d512e33fa0a11ff2c9034c74672
De Roover Justin <justinderoover@gmail.com>
2023-10-24 19:09:35 +0200

    f

--- audioData.js
+++ audioData.js
@@ -2,27 +2,27 @@ const audioTracks = [
   {
     artist: "Jewel Usain",
     title: "J'arrive",
-    source: "images/audio/Jarrive.mp3",
+    source: "audio/Jarrive.mp3",
   },
   {
     artist: "Angèle [IA]",
     title: "Saiyan",
-    source: "images/audio/Saiyan.m4a",
+    source: "audio/Saiyan.m4a",
   },
   {
     artist: "Bad Bunny",
     title: "Monaco",
-    source: "images/audio/Monaco.m4a",
+    source: "audio/Monaco.m4a",
   },
   {
     artist: "Bad Bunny",
     title: "Dakiti",
-    source: "images/audio/Dakiti.m4a",
+    source: "audio/Dakiti.m4a",
   },
   {
     artist: "Freeze Corleone",
     title: "Fraude",
-    source: "images/audio/Fraude.m4a",
+    source: "audio/Fraude.m4a",
   },
 ];
 export default audioTracks;
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,24 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
+import copy from 'rollup-plugin-copy';
 
 export default defineConfig({
-  base: "/music-player/",
-  assetsInclude: ["audio"], // Ajoutez 'audio' pour inclure le répertoire audio
+  base: '/music-player/', // Remplacez '/chemin-de-votre-application/' par la base souhaitée
+
+  build: {
+    rollupOptions: {
+      output: {
+        dir: 'dist',
+      },
+    },
+  },
+
+  plugins: [
+    // Plugin pour copier le répertoire "audio" dans le répertoire de sortie
+    copy({
+      targets: [
+        { src: 'audio', dest: 'dist' },
+      ],
+      hook: 'writeBundle',
+    }),
+  ],
 });
