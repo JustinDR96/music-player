37e6df086f4ffa4a8f8be6a41d8153df59902eae
Justin <justinderoover@gmail.com>
2023-11-15 12:09:00 +0100

    final

--- src/components/PlayerButtons.jsx
+++ src/components/PlayerButtons.jsx
@@ -68,27 +68,27 @@ const PlayerButtons = ({
   return (
     <div className="button">
       <button className="prev-next" id="prev" onClick={handlePrevious}>
-        <img src="/images/previous.png" alt="Previous" id="previous" />
+        <img src="./images/previous.png" alt="Previous" id="previous" />
       </button>
       <button
         className="play-pause"
         onClick={isPlaying ? handlePause : handlePlay}
       >
         <img
-          src="/images/play.png"
+          src="./images/play.png"
           alt="Play"
           id="play"
           style={{ display: playVisible ? "inline-block" : "none" }}
         />
         <img
-          src="/images/pause.png"
+          src="./images/pause.png"
           alt="Pause"
           id="pause"
           style={{ display: pauseVisible ? "inline-block" : "none" }}
         />
       </button>
       <button className="prev-next" id="next" onClick={handleNext}>
-        <img src="/images/next.png" alt="Next" id="next-btn" />
+        <img src="./images/next.png" alt="Next" id="next-btn" />
       </button>
     </div>
   );
