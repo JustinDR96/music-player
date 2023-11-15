1bfc06d352ecec6b216d960c8e719a05c82c1f56
Justin <justinderoover@gmail.com>
2023-11-15 10:19:13 +0100

    resolve path?

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,11 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import { viteStaticCopy } from "vite-plugin-static-copy";
 
-// [FIXME] fix import path in destination assets 
+// [FIXME] fix import path in destination assets
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/music-player/",
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        { src: "/public/audio", dest: "assets/" },
-        { src: "/public/images", dest: "assets/" },
-      ],
-      flatten: false,
-    }),
-  ],
+  plugins: [react()],
   resolve: {
     extensions: [".jsx", ".json", ".scss", ".vue"],
   },
